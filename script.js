410697e405135c9d6d184aab8ed28568eefd9ae4
kevin.lunardon <palmeirassemmundial333@gmail.com>
2025-04-12 16:50:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function whats() {
+    window.open("https://wa.me/5511953708588", "_self");
+}
+
+function insta() {
+    window.open("https://instagram.com/kevin.lunardon", "_self");
+}
+
+function linke() {
+    window.open("https://linkedin.com/in/kevinlunardon", "_self");
+}
